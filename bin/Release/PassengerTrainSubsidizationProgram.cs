baf6b4e3ce9db4412defa7d03b718386b04d1fd1
Henry F <henryfeng.developer@gmail.com>
2022-08-27 15:27:59 -0400

    Update 1.1.3
    Settings, word change and debug

---
+++ bin/Release/PassengerTrainSubsidizationProgram.cs
@@ -0,0 +1,143 @@
+﻿using UnityModManagerNet;
+using UnityEngine;
+using HarmonyLib;
+using System.Reflection;
+using PassengerJobsMod;
+using DV.Logic.Job;
+
+
+namespace Henry.Passenger_Train_Subsidization_Program
+{
+    public class PassengerTrainSubsidizationProgram
+    {
+        public enum ExtraBonusMode
+        {
+            Match,
+            Fixed
+        }
+        public static bool ModState { get; private set; }
+        public static UnityModManager.ModEntry Entry { get; private set; }
+        public static Settings.Settings thisSetting;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.Logger.Log("PTSP Load Called");
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            thisSetting = UnityModManager.ModSettings.Load<Settings.Settings>(modEntry);
+            Entry = modEntry;
+            Entry.Logger.Log("PTSP Load Complete");
+            return true;
+        }
+
+        public static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Entry.Logger.Log($"OnSaveGUI called on {modEntry.Info.DisplayName}");
+            thisSetting.Save(modEntry);
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            Entry.Logger.Log($"OnGUI called on {modEntry.Info.DisplayName}");
+            thisSetting.Draw(modEntry);
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Entry.Logger.Log($"OnToggle called on {modEntry.Info.DisplayName} for {value}");
+            ModState = value;
+            if (value)
+            {
+                AddToJobComplete();
+            }
+            else
+            {
+                RemoveFromJobComplete();
+            }
+            return true;
+        }
+
+        public static bool IsPaxJobInUse()
+        {
+            Entry.Logger.Log("Looking for passenger job mod");
+            UnityModManager.ModEntry PaxMod = UnityModManager.FindMod("PassengerJobs");
+            if (PaxMod != null)
+            {
+                Entry.Logger.Log("Found it");
+                return true;
+            }
+            else
+            {
+                Entry.Logger.Log("Did not locate it");
+                return false;
+            }
+
+        }
+
+        public static void AddToJobComplete()
+        {
+            Entry.Logger.Log("Adding to Job Complete");
+            if (IsPaxJobInUse())
+            {
+                Entry.Logger.Log("Patching now");
+                var harmony = new HarmonyLib.Harmony(Entry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                Entry.Logger.Log("Not patching now");
+                FoundNoPaxJob();
+            }
+        }
+
+        public static void RemoveFromJobComplete()
+        {
+            Debug.Log("Removing From Job Complete");
+            var harmony = new HarmonyLib.Harmony(Entry.Info.Id);
+            harmony.UnpatchAll();
+        }
+
+        public static void FoundNoPaxJob()
+        {
+            Entry.Logger.Log("Pax job not in use, this mod will not activate");
+        }
+
+        public static float GetNewIncome(Job job)
+        {
+            float income = job.GetBasePaymentForTheJob() + job.GetBonusPaymentForTheJob();
+            float newincome = IncomeModificator(thisSetting.BonusMode, income);
+
+            Entry.Logger.Log($"you can get this much with the subsidization program: {newincome}");
+            return newincome;
+        }
+
+        public static float IncomeModificator(ExtraBonusMode mode, float income)
+        {
+            Entry.Logger.Log($"Using {mode.GetType().Name} to give out subsidy with base income of: {income}");
+            switch (mode)
+            {
+                case ExtraBonusMode.Match:
+                    return income * (1f + thisSetting.MatchLevel);
+                case ExtraBonusMode.Fixed:
+                    return income + thisSetting.GiveLevel;
+            }
+            return income;
+        }
+
+        [HarmonyPatch(typeof(Job), "GetWageForTheJob")]
+        public class JobGetWageForTheJobPatch
+        {
+            public static void Postfix(Job __instance, ref float __result)
+            {
+                Job job = __instance;
+                Entry.Logger.Log($"Found job {__instance.ID}, its type code is: {__instance.jobType} | for refernce, passenger commuter job code is {PassJobType.Commuter}, passenger express job code is {PassJobType.Express}");
+                if (job.jobType == PassJobType.Commuter | job.jobType == PassJobType.Express)
+                {
+                    __result = GetNewIncome(job);
+                }
+                Entry.Logger.Log("PTSP GetWageForTheJob Done");
+            }
+        }
+    }
+}
---
+++ bin/Release/Settings.cs
@@ -0,0 +1,32 @@
+﻿using UnityModManagerNet;
+using UnityEngine;
+using System;
+
+
+namespace Henry.Passenger_Train_Subsidization_Program.Settings
+{
+    public class Settings : UnityModManager.ModSettings, IDrawable
+    {
+        [Draw("Matching Level - if Bonus Mode is set to match (new wages = original total * (1 + MatchLevel))")]
+        public float MatchLevel = 1f;
+
+        [Draw("Give Level - hand you money each time you complete a job (new wages = original total + Give Level)")]
+        public float GiveLevel = 10000f;
+
+        [Draw("Extra Bonus Mode - Give for giving out a fixed amount, Match for the bonus program to give based on based pay")]
+        public PassengerTrainSubsidizationProgram.ExtraBonusMode BonusMode = PassengerTrainSubsidizationProgram.ExtraBonusMode.Match;
+
+        public void OnChange()
+        {
+            Debug.LogWarning($"Update Aquired, will be in effect when payment is calculated again, new settings: Match Level = {MatchLevel};" +
+                $" Give Level = {GiveLevel}; Bonus Mode = {Enum.GetName(typeof(PassengerTrainSubsidizationProgram.ExtraBonusMode), BonusMode)/*Displays Bonus Mode name*/}");
+            PassengerTrainSubsidizationProgram.Entry.Logger.Log("Updated setting recevied");
+        }
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            PassengerTrainSubsidizationProgram.Entry.Logger.Log("Mod is saving its settings");
+            Save(this, modEntry);
+        }
+    }
+}
